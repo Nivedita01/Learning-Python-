db6ea3314d5005bd11bb7c224873da071d182ec3
nhari <hari.nivedita@gmail.com>
2020-09-30 21:18:10 -0400

    Added required validations for year and month

--- basic_calendar.py
+++ basic_calendar.py
@@ -4,4 +4,13 @@ yy = int(input("Enter year: "))
 mm = int(input("Enter month: "))  
   
 # display the calendar  
-print(calendar.month(yy,mm))  
+
+if((yy >= 1 and yy <= 9999) and ( mm >= 1 and mm <= 12)):
+    print(calendar.month(yy,mm))  
+elif((yy < 1 or yy > 9999) and (mm < 1 or mm > 12)):      
+    print "Enter valid year and month"
+elif(yy < 1 or yy > 9999):
+    print "Enter valid year"
+elif(mm < 1 or mm > 12):
+    print "Enter valid month"
+  
